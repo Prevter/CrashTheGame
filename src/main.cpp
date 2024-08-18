19ed56ac863fd11b2481795ee4a5c85a9b9db3b8
Oleksandr Nemesh <prevter@gmail.com>
2024-08-18 03:34:21 +0300

    test macos stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Geode/Geode.hpp>
 #include "CrashPickerLayer.hpp"
 
+constexpr size_t vector_size = sizeof(std::vector);
+constexpr size_t map_size = sizeof(std::map);
+constexpr size_t umap_size = sizeof(std::unordered_map);
+constexpr size_t set_size = sizeof(std::set);
+constexpr size_t uset_size = sizeof(std::unordered_set);
+
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(MyMenuLayer, MenuLayer) {
 	bool init() {
 		if (!MenuLayer::init()) {
 			return false;
 		}
+
+		static_assert(vector_size == 20);
+		static_assert(map_size == 20);
+		static_assert(umap_size == 20);
+		static_assert(set_size == 20);
+		static_assert(uset_size == 20);
 
 		auto crashButton = CCMenuItemSpriteExtra::create(
 			CCSprite::createWithSpriteFrameName("GJ_freeStuffBtn_001.png"),
